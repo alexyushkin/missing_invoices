7b284953aeeea50e4f64f57b183b4774bbd6f2d5
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-11-07 13:42:40 -0500

    Update missing_invoices_viz.py

--- missing_invoices_viz.py
+++ missing_invoices_viz.py
@@ -60,8 +60,8 @@ def download_aws_object(bucket, key):
 
     prim_color = st.config.get_option('theme.primaryColor') or '#f43365'
     bg_color = st.config.get_option('theme.backgroundColor') or '#000000'
-    btn_bg_color = st.config.get_option('theme.buttonBackgroundColor') or '#f3f6fc'
-    border_color = st.config.get_option('theme.buttonBorderColor') or '#dadfe7'
+#     btn_bg_color = '#f3f6fc'
+#     border_color = '#dadfe7'
     sbg_color = st.config.get_option('theme.secondaryBackgroundColor') or '#f1f3f6'
     txt_color = st.config.get_option('theme.textColor') or '#000000' 
     font = st.config.get_option('theme.font') or 'sans serif'  
@@ -69,7 +69,7 @@ def download_aws_object(bucket, key):
     custom_css = f"""
         <style>
             #{button_id} {{
-                background-color: {btn_bg_color};
+                background-color: #f3f6fc;
                 color: {txt_color};
                 padding: 0.25rem 0.75rem;
                 position: relative;
@@ -77,7 +77,7 @@ def download_aws_object(bucket, key):
                 border-radius: 0.25rem;
                 border-width: 1px;
                 border-style: solid;
-                border-color: {border_color};
+                border-color: #dadfe7;
                 border-image: initial;
                 filter: brightness(105%);
                 justify-content: center;
