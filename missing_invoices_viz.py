45f6134e6a62170e0da47b0227313a92324c16f4
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-11-07 13:23:58 -0500

    Update missing_invoices_viz.py

--- missing_invoices_viz.py
+++ missing_invoices_viz.py
@@ -60,8 +60,8 @@ def download_aws_object(bucket, key):
 
     prim_color = st.config.get_option('theme.primaryColor') or '#F43365'
 #     bg_color = st.config.get_option('theme.backgroundColor') or '#000000'
-    btn_bg_color = '#f3f6fc'
-    border_color = '#dadfe7'
+    btn_bg_color = '#F3F6FC'
+    border_color = '#DADFE7'
     sbg_color = st.config.get_option('theme.secondaryBackgroundColor') or '#f1f3f6'
     txt_color = st.config.get_option('theme.textColor') or '#000000' 
     font = st.config.get_option('theme.font') or 'sans serif'  
