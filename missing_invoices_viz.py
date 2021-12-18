a0a1332498dec49501e69615b9c8206595d41f4d
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-12-18 17:48:46 -0500

    Update missing_invoices_viz.py

--- missing_invoices_viz.py
+++ missing_invoices_viz.py
@@ -270,10 +270,10 @@ try:
     obj = s3.get_object(Bucket=bucket, Key=file_name)
     df2 = pd.read_excel(io.BytesIO(obj['Body'].read()), engine='openpyxl', sheet_name='hea_invoices', 
 			parse_dates=['TS_HEA_Invoice_Submitted__c', 'Activity_Date__c'], converters={'Netsuite_Customer_ID__c': str})
-#     try: 
-# 	df2['Netsuite_Customer_ID__c'] = df2['Netsuite_Customer_ID__c'].fillna(0).astype('int').astype('str')
-#     except:
-# 	pass
+    try: 
+        df2['Netsuite_Customer_ID__c'] = df2['Netsuite_Customer_ID__c'].fillna(0).astype('int').astype('str')
+    except:
+        pass
 except Exception as e:
     print(e)
 
@@ -282,9 +282,9 @@ try:
     df3 = pd.read_excel(io.BytesIO(obj['Body'].read()), engine='openpyxl', sheet_name='wx_invoices', parse_dates=['Completion_Walk_Date__c'],
 		        converters={'Netsuite_Customer_ID__c': str})
     try:
-	df3['Netsuite_Customer_ID__c'] = df3['Netsuite_Customer_ID__c'].fillna(0).astype('int').astype('str')
+        df3['Netsuite_Customer_ID__c'] = df3['Netsuite_Customer_ID__c'].fillna(0).astype('int').astype('str')
     except:
-	pass
+        pass
 except Exception as e:
     print(e)
 
@@ -293,9 +293,9 @@ try:
     df4 = pd.read_excel(io.BytesIO(obj['Body'].read()), engine='openpyxl', sheet_name='hvac_invoices', parse_dates=['Last_Install_Completion_Date__c'], 
 			converters={'Netsuite_Customer_ID__c': str})
     try:
-	df4['Netsuite_Customer_ID__c'] = df4['Netsuite_Customer_ID__c'].fillna(0).astype('int').astype('str')
+        df4['Netsuite_Customer_ID__c'] = df4['Netsuite_Customer_ID__c'].fillna(0).astype('int').astype('str')
     except:
-	pass
+        pass
 except Exception as e:
     print(e)
 
