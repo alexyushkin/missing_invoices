b692ca444691ee076ee8a439436b4179e21c6109
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-12-26 08:50:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import pandas as pd
 from multiapp import MultiApp
 from apps import app1, app2
 
+users = pd.read_csv('users.csv')
+names = users['name'].to_list()
+usernames = users['username'].to_list()
+hashed_passwords = users['hashed_password'].to_list()
 
-app = MultiApp()
+authenticator = stauth.authenticate(names, usernames, hashed_passwords,
+                                    'missingCustomersAndInvoices', 'p3jCB8sPxF',
+                                    cookie_expiry_days=1)
 
-app.add_app("App 1", app1.function)
-app.add_app("App 2", app2.function)
+name, authentication_status = authenticator.login('Login','sidebar')
 
-app.run_app()
+if authentication_status:
+    app = MultiApp()
+
+    app.add_app("App 1", app1.function)
+    app.add_app("App 2", app2.function)
+
+    app.run_app()
+
+elif authentication_status == False:
+    st.error('Username/Password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your Username and Password')
