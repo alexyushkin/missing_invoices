6528dd4bb122fa266ff77191d5a6bb74b951c909
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-12-26 08:44:38 -0500

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -25,16 +25,16 @@ class MultiApp:
 #             self.apps,
 #             format_func=lambda page: page['title']
 #         )
-    if st.session_state['authentication_status']:
-        st.write('Welcome *%s*' % (st.session_state['name']))
-            if st.session_state['name'] == 'Test':
-                page['App 1']()
-            if st.session_state['name'] == 'Test':
-                page['App 2']()
-    elif st.session_state['authentication_status'] == False:
-        st.error('Username/password is incorrect')
-    elif st.session_state['authentication_status'] == None:
-        st.warning('Please enter your username and password')
+        if st.session_state['authentication_status']:
+            st.write('Welcome *%s*' % (st.session_state['name']))
+                if st.session_state['name'] == 'Test':
+                    page['App 1']()
+                if st.session_state['name'] == 'Test':
+                    page['App 2']()
+        elif st.session_state['authentication_status'] == False:
+            st.error('Username/password is incorrect')
+        elif st.session_state['authentication_status'] == None:
+            st.warning('Please enter your username and password')
 
         # run the app function
 #         page['function']()
