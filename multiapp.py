8c349a8c887ee825e92d0f0f1ce4a88d1a13ef85
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-12-26 18:31:07 -0500

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -1,17 +1,11 @@
 import streamlit as st
-import streamlit_authenticator as stauth
-
 
 class MultiApp:
-
-
+    
     def __init__(self) -> None:
-
         self.apps = []
 
-
     def add_app(self, title, function) -> None:
-
         self.apps.append(
             {
                 'title': title,
@@ -20,11 +14,9 @@ class MultiApp:
         )
 
     def run_app(self):
-
         page = st.sidebar.selectbox(
             'Select Application',
             self.apps,
             format_func=lambda page: page['title']
         )
-
         page['function']()
