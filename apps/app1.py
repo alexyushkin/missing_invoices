5fa82fe396161ec3476338897c86cd818a9c36c6
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-12-26 09:20:02 -0500

    Update app1.py

--- apps/app1.py
+++ apps/app1.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 
-def function():
-    st.markdown("## This is App 1")
+# def function():
+st.markdown("## This is App 1 for Test")
 
-    st.write("\n")
+st.write("\n")
 
-    if st.button("Ok"):
-
-        st.markdown("**You hit Ok button**")
+if st.button("Ok"):
+    st.markdown("**You hit Ok button**")
