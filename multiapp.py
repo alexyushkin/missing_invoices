8a451f80d49ef9d695107329b2f78a849b64cfcb
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-12-26 08:36:40 -0500

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -20,11 +20,21 @@ class MultiApp:
 
     def run_app(self):
 
-        page = st.sidebar.selectbox(
-            'Applications',
-            self.apps,
-            format_func=lambda page: page['title']
-        )
+#         page = st.sidebar.selectbox(
+#             'Applications',
+#             self.apps,
+#             format_func=lambda page: page['title']
+#         )
+    if st.session_state['authentication_status']:
+        st.write('Welcome *%s*' % (st.session_state['name']))
+            if st.session_state['name'] == 'Test':
+                page['App 1']() 
+            if st.session_state['name'] == 'Test':
+                page['App 2']() 
+    elif st.session_state['authentication_status'] == False:
+        st.error('Username/password is incorrect')
+    elif st.session_state['authentication_status'] == None:
+        st.warning('Please enter your username and password')
 
         # run the app function
-        page['function']()
+#         page['function']()
