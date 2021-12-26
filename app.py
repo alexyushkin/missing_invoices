9b368571ea01730381276fd6aa5effe7bf770047
Alexey Yushkin <AYushkin2019@student.hult.edu>
2021-12-26 09:16:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ if authentication_status:
     if st.session_state['authentication_status']:
         st.sidebar.write('Welcome, *%s!*' % (st.session_state['name']))
         if st.session_state['name'] == 'Test':
-            app1
+            app1.function
         if st.session_state['name'] == 'Alexey':
-            app2
+            app2.function
     elif st.session_state['authentication_status'] == False:
         st.error('Username/password is incorrect')
     elif st.session_state['authentication_status'] == None:
